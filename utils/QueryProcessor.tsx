443600380ed1711efa5ab568b0ab28711ffdd68d
Levi Crosby <lcrosby@andrew.cmu.edu>
2024-03-14 15:41:33 -0400

    what is your andrewid

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("What is your Andrew ID?")) {
+    return (
+      "lcrosby"
+    );
+  }
+
   return "";
 }
